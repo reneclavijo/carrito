73badf161b5e6482fb33ea974fbd6333fab36249
rene rodriguez <reneclavijo@live.com>
2021-08-31 11:10:40 -0500

    corregir el boton para aumentar cantidad productos en carrito

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -20,6 +20,16 @@ class CarrosController < ApplicationController
         redirect_to root_path
     end
 
+    # POST carros/:id_producto/cantidad
+    def aumentar_cantidad_producto
+        producto_para_agregar = Producto.find(params[:id_producto])
+        contenido_carrito = @carro.carros_contenidos.find_by(producto: producto_para_agregar)
+        contenido_carrito.cantidad += 1
+        contenido_carrito.save
+
+        redirect_to carrito_path
+    end
+
     # DELETE
     def eliminar_producto
         @carro.carros_contenidos.find_by(producto_id: params[:id_producto]).destroy
