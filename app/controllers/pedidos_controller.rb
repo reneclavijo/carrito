3952692561f69e5141337b3ca78890243871705c
rene rodriguez <reneclavijo@live.com>
2021-09-01 13:46:32 -0500

    mostrando los errores del formulario para crear producto

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -17,23 +17,42 @@ class PedidosController < ApplicationController
         # eliminar el carrito cuando todo termine
 
         @datos_envio = DatosEnvio.new(params_datos_envio)
-        if @datos_envio.save
-            @pedido = Pedido.new(
-                codigo: SecureRandom.hex(4).upcase,
-                total: @carro.total,
-                destino_id: params_destino[:destino_id],
-                estados_pedido: EstadosPedido.find_by(estado: 'solicitado'),
-                datos_envio: @datos_envio
-            )
-            if @pedido.save
-                redirect_to action: :crear
+        
+        if params_destino[:destino_id].empty? == false
+
+            if @datos_envio.save
+                @pedido = Pedido.new(
+                    codigo: SecureRandom.hex(4).upcase,
+                    total: @carro.total,
+                    destino_id: params_destino[:destino_id],
+                    estados_pedido: EstadosPedido.find_by(estado: 'solicitado'),
+                    datos_envio: @datos_envio
+                )
+                if @pedido.save
+                    
+                    @carro.carros_contenidos.each do |contenido|
+                        DetallesPedido.create(
+                            pedido: @pedido,
+                            producto: contenido.producto,
+                            cantidad: contenido.cantidad
+                        )
+                    end
+
+
+                    redirect_to action: :crear
+                else
+                    @destinos = Destino.select(:id, :nombre).order(nombre: :asc)
+                    render :crear
+                end
             else
                 @destinos = Destino.select(:id, :nombre).order(nombre: :asc)
-                render :crear
+                render :crear            
             end
+
         else
+            @datos_envio.errors.add(:destino_id, "Seleccione un destino")
             @destinos = Destino.select(:id, :nombre).order(nombre: :asc)
-            render :crear            
+            render :crear 
         end
     end
 
