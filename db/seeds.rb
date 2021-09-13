d166238bd74844394c9a185e2c2884bba946ee9b
rene rodriguez <reneclavijo@live.com>
2021-09-13 13:03:05 -0500

    agreagndo pregunta en el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,42 @@
-EstadosProducto.create([
-    { estado: 'activo' },
-    { estado: 'inactivo' }
-])
+if EstadosProducto.count.zero?
+    EstadosProducto.create([
+        { estado: 'activo' },
+        { estado: 'inactivo' }
+    ])
+end
 
-TiposPago.create([
-    {pago: 'efectivo'},
-    {pago: 'tarjeta'}
-])
+if TiposPago.count.zero?
+    TiposPago.create([
+        {pago: 'efectivo'},
+        {pago: 'tarjeta'}
+    ])
+end
 
-EstadosPedido.create([
-    {estado: 'solicitado'},
-    {estado: 'enviado'},
-    {estado: 'entregado'}
-])
+if EstadosPedido.count.zero?
+    EstadosPedido.create([
+        {estado: 'solicitado'},
+        {estado: 'enviado'},
+        {estado: 'entregado'}
+    ])
+end
 
-Region.create([
-    { nombre: 'Primera Region' },
-    { nombre: 'Segunda Region' },
-    { nombre: 'Tercera Region' },
-    { nombre: 'Cuarta Region' },
-    { nombre: 'Quinta Region' },
-    { nombre: 'Sexta Region' },
-    { nombre: 'Septima Region' },
-    { nombre: 'Octava Region' },
-    { nombre: 'Novena Region' },
-    { nombre: 'Décima Region' },
-    { nombre: 'Décima Primera Region' },
-    { nombre: 'Décima Segunda Region' },
-    { nombre: 'Décima Tercera Region' },
-    { nombre: 'Décima Cuarta Region' },
-    { nombre: 'Décima Quinta Region' },
-    { nombre: 'Décima Sexta Region' }
-])
+if Region.count.zero?
+    Region.create([
+        { nombre: 'Primera Region' },
+        { nombre: 'Segunda Region' },
+        { nombre: 'Tercera Region' },
+        { nombre: 'Cuarta Region' },
+        { nombre: 'Quinta Region' },
+        { nombre: 'Sexta Region' },
+        { nombre: 'Septima Region' },
+        { nombre: 'Octava Region' },
+        { nombre: 'Novena Region' },
+        { nombre: 'Décima Region' },
+        { nombre: 'Décima Primera Region' },
+        { nombre: 'Décima Segunda Region' },
+        { nombre: 'Décima Tercera Region' },
+        { nombre: 'Décima Cuarta Region' },
+        { nombre: 'Décima Quinta Region' },
+        { nombre: 'Décima Sexta Region' }
+    ])
+end
