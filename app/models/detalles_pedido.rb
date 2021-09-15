430780ddf106241244cf1ca0b9d435e3f214b3ba
rene rodriguez <reneclavijo@live.com>
2021-09-15 10:13:52 -0500

    actualizar total del pedido

--- app/models/detalles_pedido.rb
+++ app/models/detalles_pedido.rb
@@ -1,4 +1,21 @@
 class DetallesPedido < ApplicationRecord
+
+  after_save    :actualizar_total
+  after_destroy :actualizar_total
+
   belongs_to :producto
   belongs_to :pedido
+
+  private
+
+  def actualizar_total
+    pedido = self.pedido
+    subtotal = 0
+    pedido.detalles_pedidos.each do |detalle|
+      subtotal += detalle.cantidad * detalle.producto.precio
+    end
+    pedido.total = subtotal
+    pedido.save
+  end
+  
 end
